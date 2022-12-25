0a49e26f6b70ed509a3a21d696c52b76eb2c44f2
KR-BOTZ <119985743+KR-BOTZ@users.noreply.github.com>
2022-12-26 00:09:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/KR-BOTZ/upo2 /upo2
-cd /Uploader-Bot-V2
+git clone https://github.com/KR-BOTZ/rent-bot3 /rent-bot3
+cd /rent-bot3
 pip3 install -U -r requirements.txt
 echo "Starting Bot.... Please Wait. Check You Log. Bot Credit @DKBOTZ\n Special Credit :- @Tellybots"
 python3 bot.py
