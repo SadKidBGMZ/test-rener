315bf192aa9689fb4cb4b21c8cd39a4184f66793
KR-BOTZ <119985743+KR-BOTZ@users.noreply.github.com>
2022-12-31 06:23:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import asyncio
 from pyrogram import Client, compose,idle
 import os
-
+from config import Config
 from plugins.cb_data import app as Client2
 
 TOKEN = os.environ.get("TOKEN", "5377834462:AAFr_4q9Sw3RtGPoMtgkxy-w4A6TE8oN_0w")
@@ -10,7 +10,7 @@ API_ID = int(os.environ.get("API_ID", "13384432"))
 
 API_HASH = os.environ.get("API_HASH", "ea9db4503ed7088b788e06dfd818e00e")
 
-STRING = os.environ.get("STRING", "AQDHcnQAjG81efKGAqsDjG6mJrncMF9Tqb7HmWM-9tFDt8Ot4UXEQPaLyarOVd_voUfS05xzZ8ZdVQGr6CORALmLyWSBm7SoV0TxIFxYns4hM-EjithAal1BhyR2HFLwxZiwVpxkYcuLVdI3a87KFJkdORgBNAPwgpz2Q9lNHZTEJmr1a1QMj7t2vakuB96IBwbBCbwCgsNrqBCBxEhbabi8n6PcxxhW39LzMQhj9Ac_NRX8Ak2un9L2c6drMn4qMx0DFL-RDsSh4UWA_UzNol3-R5STVXw0iBaxNCyTUbsNEiX_lYXPWPk3QZ2z2Rs1w3mKpIdGzzipnmKffP-Gg9iJqlZjPwAAAAFB5XkyAA") 
+STRING = os.environ.get("Config.STRING_SESSION", "AQDHcnQAjG81efKGAqsDjG6mJrncMF9Tqb7HmWM-9tFDt8Ot4UXEQPaLyarOVd_voUfS05xzZ8ZdVQGr6CORALmLyWSBm7SoV0TxIFxYns4hM-EjithAal1BhyR2HFLwxZiwVpxkYcuLVdI3a87KFJkdORgBNAPwgpz2Q9lNHZTEJmr1a1QMj7t2vakuB96IBwbBCbwCgsNrqBCBxEhbabi8n6PcxxhW39LzMQhj9Ac_NRX8Ak2un9L2c6drMn4qMx0DFL-RDsSh4UWA_UzNol3-R5STVXw0iBaxNCyTUbsNEiX_lYXPWPk3QZ2z2Rs1w3mKpIdGzzipnmKffP-Gg9iJqlZjPwAAAAFB5XkyAA") 
 
 
 
