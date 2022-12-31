4c1f03af162e5cec5c73f92a09688ac423705cb8
KR-BOTZ <119985743+KR-BOTZ@users.noreply.github.com>
2022-12-31 05:53:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ echo "Cloning Repo...."
 git clone https://github.com/KR-BOTZ/rent-bot3 /rent-bot3
 cd /rent-bot3
 pip3 install -U -r requirements.txt
-echo "Starting Bot.... Please Wait. Check You Log. Bot Credit @DKBOTZ\n Special Credit :- @Tellybots"
+echo "Starting Bot.... "
 python3 bot.py
