7f70a829c5fda5abec8e22ec116860a50c5d7156
SadKidBGMZ <104976581+SadKidBGMZ@users.noreply.github.com>
2023-01-10 00:14:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,15 @@
 import asyncio
 from pyrogram import Client, compose,idle
 import os
-from config import Config
+from config import BOT_TOKEN, API_ID, API_HASH, STRING
 from plugins.cb_data import app as Client2
-
-TOKEN = os.environ.get("TOKEN", "5377834462:AAFr_4q9Sw3RtGPoMtgkxy-w4A6TE8oN_0w")
-
-API_ID = int(os.environ.get("API_ID", "13384432"))
-
-API_HASH = os.environ.get("API_HASH", "ea9db4503ed7088b788e06dfd818e00e")
-
-STRING = "Config.STRING_SESSION"
 
 
 bot = Client(
 
            "Renamer",
 
-           bot_token=TOKEN,
+           bot_token=BOT_TOKEN,
 
            api_id=API_ID,
 
