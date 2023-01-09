cf2b7b010af7bcd64b293c67456a2406df1a7440
KR-BOTZ <119985743+KR-BOTZ@users.noreply.github.com>
2023-01-09 23:00:34 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,6 +1,7 @@
 import os 
 from pyrogram import Client, filters
-token = os.environ.get('TOKEN','5377834462:AAFr_4q9Sw3RtGPoMtgkxy-w4A6TE8oN_0w')
+from config import BOT_TOKEN
+token = "BOT_TOKEN"
 botid = token.split(':')[0]
 from helper.database import botdata, find_one, total_user
 from helper.progress import humanbytes
