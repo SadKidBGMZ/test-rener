d2944e0b39230af29faaed5c278ae791575de238
SadKidBGMZ <104976581+SadKidBGMZ@users.noreply.github.com>
2023-01-10 18:44:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,19 @@
-import re
-from os import environ
+from os 
 
 # Bot information
 
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
-STRING = environ['STRING_SESSION']
+API_ID = int(os.environ.get('API_ID'))
+API_HASH = os.environ.get('API_HASH') 
+BOT_TOKEN = os.environ.get('BOT_TOKEN','') 
+STRING = os.environ.get("STRING_SESSION","")
 
 # Admins, Channels & Users
 
-ADMIN = int(environ['ADMIN', 1504797855])
-CHANNEL = environ['AUTH_CHANNEL']
-LOG_CHANNEL = environ["LOG_CHANNEL"]
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL",""))
+ADMIN = int(os.environ.get("ADMIN", 923943045))
+CHANNEL = os.environ.get('AUTH_CHANNEL',"")
 
 # MongoDB information
 
-DB_URL = environ['DATABASE_URL', 'mongodb+srv://dkbotzon:dkbotzon@cluster0.q9dkemw.mongodb.net/?retryWrites=true&w=majority']
-DB_NAME = environ['DATABASE_NAME', 'MrTamilKiD']
+DB_URL = os.environ.get('DATABASE_URL', 'mongodb+srv://dkbotzon:dkbotzon@cluster0.q9dkemw.mongodb.net/?retryWrites=true&w=majority') 
+DB_NAME = os.environ.get('DATABASE_NAME', 'MrTamilKiD') 
