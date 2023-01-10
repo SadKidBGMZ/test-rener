ce931ef8ed42dec3fb24daf23feca85f7e9720e7
SadKidBGMZ <104976581+SadKidBGMZ@users.noreply.github.com>
2023-01-10 19:36:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ import os
 # Bot information
 
 API_ID = int(os.environ.get('API_ID'))
-API_HASH = os.environ.get('API_HASH') 
-BOT_TOKEN = os.environ.get('BOT_TOKEN','') 
+API_HASH = os.environ.get('API_HASH','ea9db4503ed7088b788e06dfd818e00e') 
+BOT_TOKEN = os.environ.get('BOT_TOKEN','5623538837:AAFp2qbObz2KgpMSX5By5ai5Xu2FQOnz0xk') 
 STRING = os.environ.get("STRING_SESSION","")
 
 # Admins, Channels & Users
 
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL",""))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL","-1001551766281"))
 ADMIN = int(os.environ.get("ADMIN", 923943045))
 CHANNEL = os.environ.get('AUTH_CHANNEL',"")
 
